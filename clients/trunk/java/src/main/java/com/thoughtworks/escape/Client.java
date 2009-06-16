e3af803c5c56e7fd626bafc0717dbb1861ecfe7d
ken.m.friesen <ken.m.friesen@59ab4f38-e3ec-11dd-a8eb-ddf07f3c1838>
2009-06-16 15:11:47 +0000

    Refactored tests to make them more readable, added javadoc and eclipse project files

--- clients/trunk/java/src/main/java/com/thoughtworks/escape/Client.java
+++ clients/trunk/java/src/main/java/com/thoughtworks/escape/Client.java
@@ -1,30 +1,43 @@
+/*
+ *   Copyright 2009 ThoughtWorks
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package com.thoughtworks.escape;
 
-/*
-#   Copyright 2009 ThoughtWorks
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-*/
-
-import java.util.Properties;
-import java.net.URL;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
 
 public class Client {
-    public static Properties getProperties(String host, String env, String app) throws IOException {
-        URL url = new URL(host + "/environments/" + env + "/" + app);
-        Properties props = new Properties();
-        props.load(url.openStream());
-        return props;
-    }
+
+	/**
+	 * @param escapeBaseUrl	the Escape server's base URL, e.g. http://escape-server:7000 
+	 * @param environmentName	the name of the environment  
+	 * @param applicationName	the name of the application
+	 * @return	the <code>Properties</code> object for environment <code>environmentName</code>
+	 * and application <code>applicationName</code> 
+	 * @throws IOException
+	 */
+	public static Properties getProperties(String escapeBaseUrl, String environmentName, String applicationName) 
+			throws IOException {
+		return getProperties(new URL(escapeBaseUrl + "/environments/" + environmentName + "/" + applicationName));
+	}
+
+	private static Properties getProperties(URL url) throws IOException {
+		Properties props = new Properties();
+		props.load(url.openStream());
+		return props;
+	}
 }
--- clients/trunk/java/src/test/java/com/thoughtworks/escape/ClientTest.java
+++ clients/trunk/java/src/test/java/com/thoughtworks/escape/ClientTest.java
@@ -1,125 +1,112 @@
 package com.thoughtworks.escape;
 
 /*
-#   Copyright 2009 ThoughtWorks
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-*/
+ #   Copyright 2009 ThoughtWorks
+ #
+ #   Licensed under the Apache License, Version 2.0 (the "License");
+ #   you may not use this file except in compliance with the License.
+ #   You may obtain a copy of the License at
+ #
+ #       http://www.apache.org/licenses/LICENSE-2.0
+ #
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS,
+ #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ #   See the License for the specific language governing permissions and
+ #   limitations under the License.
+ */
 
-import org.junit.Test;
-import org.junit.Before;
-import static org.junit.matchers.JUnitMatchers.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
+import java.util.Properties;
+
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PutMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-
-import java.util.Properties;
-import java.io.IOException;
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.ConnectException;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ClientTest {
-    private static final String HOST = "http://localhost:7000";
-    private static final String ENV = "default";
-    private static final String APP = "esc-client-java";
+	
+	private static HttpClient HTTP_CLIENT = new HttpClient();
+	
+	private static final String HOST = "http://localhost:7000";
+	private static final String ENVIRONMENT_NAME = "default";
+	private static final String APPLICATION_NAME = "esc-client-java";
 
-    private static String getUrl() {
-        return HOST + "/environments/" + ENV + "/" + APP;
-    }
+	private static final String escapeBaseUrl = HOST + "/environments/";
+	private static final String escapeApplicationBaseUrl = escapeBaseUrl + ENVIRONMENT_NAME + "/" + APPLICATION_NAME;
 
-    @Before
-    public void setUpTestData() throws IOException {
-        HttpClient client = new HttpClient();
-        HttpMethod method = new GetMethod(HOST);
-        int code = client.executeMethod(method);
-        assertEquals(200, code);
+	@Test
+	public void testCanGetPropertiesFromEscapeServer() throws IOException {
+		Properties properties = Client.getProperties(HOST, ENVIRONMENT_NAME, APPLICATION_NAME);
 
-        method = new PutMethod(getUrl());
-        code = client.executeMethod(method);
-        assertThat(code, either(is(200)).or(is(201)));
+		assertTrue(properties.containsKey("key1"));
+		assertTrue(properties.containsKey("key2"));
+		assertTrue(properties.getProperty("key1").equals("value1"));
+		assertTrue(properties.getProperty("key2").equals("value2"));
+	}
 
-        PutMethod put = new PutMethod(getUrl() + "/key1");
-        put.setRequestBody("value1");
-        code = client.executeMethod(put);
-        assertThat(code, either(is(200)).or(is(201)));
+	@Test(expected=ConnectException.class)
+	public void testThatConnectExceptionIsThrownWhenServerIsDown() throws IOException {
+		Client.getProperties("http://localhost:700", ENVIRONMENT_NAME, APPLICATION_NAME);
+	}
 
-        put = new PutMethod(getUrl() + "/key2");
-        put.setRequestBody("value2");
-        code = client.executeMethod(put);
-        assertThat(code, either(is(200)).or(is(201)));
-    }
+	@Test(expected=MalformedURLException.class)
+	public void testThatMalformedURLExceptionIsThrownOnBadURL() throws IOException {
+		Client.getProperties("sheep://cheese", ENVIRONMENT_NAME, APPLICATION_NAME);
+	}
 
-    @Test
-    public void testCanGetPropertiesFromEscapeServer() throws IOException {
-        Properties properties = Client.getProperties(HOST, ENV, APP);
+	@Test(expected=FileNotFoundException.class)
+	public void testThatBadEnvThrowsFileNotFoundException() throws IOException {
+		Client.getProperties(HOST, "non-existing-env", APPLICATION_NAME);
+	}
 
-        assertTrue(properties.containsKey("key1"));
-        assertTrue(properties.containsKey("key2"));
-        assertTrue(properties.getProperty("key1").equals("value1"));
-        assertTrue(properties.getProperty("key2").equals("value2"));
-    }
+	@Test(expected=FileNotFoundException.class)
+	public void testThatBadAppThrowsFileNotFoundException() throws IOException {
+		Client.getProperties(HOST, ENVIRONMENT_NAME, "non-existing-app");
+	}
+	
+	@Before
+	public void setUpTestData() throws IOException {
+		assertEscapeIsRunning();
+		addApplication();
+		addProperty("key1", "value1");
+		addProperty("key2", "value2");
+	}
 
-    @Test
-    public void testThatConnectExceptionIsThrownWhenServerIsDown() throws IOException {
-        Boolean exception = false;
+	private void assertEscapeIsRunning() {
+		try {
+			assertThat(HTTP_CLIENT.executeMethod(new GetMethod(HOST)), is(200));
+		} catch (IOException e) {
+			fail(String.format("Can't connect to Escape server at %s -- check that the escape server is running", HOST));
+		}
+	}
 
-        try {
-            Client.getProperties("http://localhost:700", ENV, APP);
-        } catch (ConnectException e) {
-            exception = true;
-        }
+	private void addApplication() {
+		try {
+			assertThat(HTTP_CLIENT.executeMethod(new PutMethod(escapeApplicationBaseUrl)), either(is(200)).or(is(201)));
+		} catch (IOException e) {
+			fail(String.format("Failed to add application to Escape using [%s]", escapeApplicationBaseUrl));
+		}
+	}
 
-        assertTrue(exception);
-    }
+	@SuppressWarnings("deprecation")
+	private void addProperty(String key, String value) {
+		final String url = escapeApplicationBaseUrl + key;
+		try {
+			PutMethod putMethod = new PutMethod(url);
+			putMethod.setRequestBody(value);
+			assertThat(HTTP_CLIENT.executeMethod(putMethod), either(is(200)).or(is(201)));
+		} catch (IOException e) {
+			fail(String.format("Failed to add property to Escape application using [%s]", url));
+		}
+	}
 
-    @Test
-    public void testThatMalformedURLExceptionIsThrownOnBadURL() throws IOException {
-        Boolean exception = false;
-        try {
-            Client.getProperties("sheep://cheese", ENV, APP);
-        } catch (MalformedURLException e) {
-            exception = true;
-        }
-
-        assertTrue(exception);
-    }
-
-    @Test
-    public void testThatBadEnvThrowsFileNotFoundException() throws IOException {
-        Boolean exception = false;
-        try {
-            Client.getProperties(HOST, "non-existing-env", APP);
-        } catch (FileNotFoundException e) {
-            exception = true;
-        }
-        
-        assertTrue(exception);
-    }
-
-    @Test
-    public void testThatBadAppThrowsFileNotFoundException() throws IOException {
-        Boolean exception = false;
-        try {
-            Client.getProperties(HOST, ENV, "non-existing-app");
-        } catch (FileNotFoundException e) {
-            exception = true;
-        }
-
-        assertTrue(exception);
-    }
 }
